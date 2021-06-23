b727d9c5c87c7f78ec53d8fda951c260dccf7582
Naomi Mango <naomi.mango@gmail.com>
2021-06-23 17:53:02 +0200

    [NEW/MAJ] Gestion des fichiers utilisateur, route DELETE

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,6 +1,9 @@
 // Import du modèle dans le contrôleur
 const Sauce = require("../models/Sauce");
 
+// Import du package dans le contrôleur
+const fs = require("fs"); // Accède aux fonctions qui permettent de modifier le système de fichiers
+
 // Import de la logique métier de la route POST
 exports.createSauce = (req, res, next) => {
     // Envoyer les données de la requête en form-data (non en JSON)
@@ -59,6 +62,24 @@ exports.modifySauce = (req, res, next) => {
         .catch((error) => {
             res.status(400).json({ error: error });
         });
+};
+
+// Import de la logique métier de la route DELETE
+exports.deleteSauce = (req, res, next) => {
+    // Utiliser l'ID reçu comme paramètre pour accéder à la Sauce correspondant dans la base de données
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            // Utiliser le segment "/images/" de notre URL d'image pour extraire le nom du fichier à supprimer
+            const filename = sauce.imageUrl.split("/images/")[1];
+            // Passer comme paramètres le fichier à supprimer et le callback à exécuter une fois ce fichier supprimé
+            fs.unlink(`images/${filename}`, () => {
+                // Implémenter la logique d'origine en supprimant la Sauce de la base de données
+                Sauce.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: "La sauce a été supprimée avec succès !" }))
+                    .catch(error => res.status(400).json({ error }));
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 // Import de la logique métier de la route GET
--- routes/sauce.js
+++ routes/sauce.js
@@ -14,6 +14,7 @@ router.get("/", auth, sauceCtrl.getAllSauces);
 router.post("/", auth, multer, sauceCtrl.createSauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 // Permettre l'export du routeur sur d'autres fichiers
 module.exports = router;
