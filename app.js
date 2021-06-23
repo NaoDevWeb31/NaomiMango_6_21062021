cde977072b97d7332095732df149d3fdbce9444f
Naomi Mango <naomi.mango@gmail.com>
2021-06-23 10:38:11 +0200

    [NEW] Création de l'appli Express

---
+++ app.js
@@ -0,0 +1,9 @@
+// Import de packages dans l'appli
+const express = require("express");
+
+// Créer l'appli
+const app = express();
+
+
+// Permettre l'export de l'appli sur d'autres fichiers
+module.exports = app;
